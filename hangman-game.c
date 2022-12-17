c0dc2e32853859256ee3d9959ca56d404bda6d6b
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2022-12-17 15:07:48 -0300

    The code was refactored

--- hangman-game.c
+++ hangman-game.c
@@ -38,6 +38,23 @@ get_letter(char *secret_letters_found)
 	return spoken_letter;
 }
 
+bool
+take_guess(const char *secret_word, char *secret_letters_found, unsigned int word_length)
+{
+	char letter;
+	bool found = false;
+
+	letter = get_letter(secret_letters_found);
+
+	for (register int i = 0; i < word_length; i++)
+		if (secret_word[i] == letter) {
+			secret_letters_found[i] = letter;
+			found = true;
+		}
+	
+	return found;
+}
+
 void
 update(char *secret_letters_found)
 {
@@ -54,27 +71,22 @@ int
 main(void)
 {
 	char secret_word[20], secret_letters_found[20];
+	unsigned int word_length;
 	bool hit = false, hung = true;
 
 	sprintf(secret_word, "Melancia");
+	word_length = strlen(secret_word);
 
-	for (register int i = 0; i < strlen(secret_word); i++)
+	for (register int i = 0; i < word_length; i++)
 		secret_letters_found[i] = '_';
-	secret_letters_found[strlen(secret_word)] = '\0';
+	secret_letters_found[word_length] = '\0';
 
 	do {
-		char letter;
-		bool found = false;
+		bool found;
 
 		update(secret_letters_found);
 
-		letter = get_letter(secret_letters_found);
-
-		for (register int i = 0; i < strlen(secret_word); i++)
-			if (secret_word[i] == letter) {
-				secret_letters_found[i] = letter;
-				found = true;
-			}
+		found = take_guess(secret_word, secret_letters_found, word_length);
 
 		if (!found && failed())
 			hung = false;
