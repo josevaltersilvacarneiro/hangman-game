6ab82de3ddb110e56592967757b5b53a44065bf2
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2022-12-20 14:09:52 -0300

    The header file needs to be corrected to conform with the main program

--- hangman-game.h
+++ hangman-game.h
@@ -3,7 +3,7 @@
 void get_secret_word(char *secret_word);
 void push_secret_word(void);
 bool failed(void);
-char get_letter(char *secret_letters_found);
+char get_letter(char *typed_letters);
 bool take_guess(const char *secret_word, char *secret_letters_found, unsigned int word_length);
 void update(char *secret_letters_found);
 void print_win(const char *secret_word);
